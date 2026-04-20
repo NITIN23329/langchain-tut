f3a5e2ac5dc034e05515e5d0a71b90a391ee3ba7
nitin23329 <nitin23329@gmail.com>
2026-04-21 00:42:42 +0530

    added personal chef

--- notebooks/module-1/1.5_personal_chef.py
+++ notebooks/module-1/1.5_personal_chef.py
@@ -1,34 +1,30 @@
 from dotenv import load_dotenv
-
 load_dotenv()
 
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+model = ChatGoogleGenerativeAI(model = 'gemini-3.1-flash-lite-preview',max_tokens=500)
+
 from langchain.tools import tool
-from typing import Dict, Any
 from tavily import TavilyClient
+from typing import Dict, Any
 
 tavily_client = TavilyClient()
 
 @tool
-def web_search(query: str) -> Dict[str, Any]:
-
-    """Search the web for information"""
-
+def web_search(query: str) -> Dict[str, Any]: 
+    """This method does web search"""
     return tavily_client.search(query)
 
-system_prompt = """
-
-You are a personal chef. The user will give you a list of ingredients they have left over in their house.
-
-Using the web search tool, search the web for recipes that can be made with the ingredients they have.
-
-Return recipe suggestions and eventually the recipe instructions to the user, if requested.
-
-"""
 
 from langchain.agents import create_agent
 
-agent = create_agent(
-    model="gpt-5-nano",
-    tools=[web_search],
-    system_prompt=system_prompt
-)
+system_prompt = """
+You are a world class chef. 
+Your task is to recommend receipies from the ingredients specified by user by searching for it on web. 
+Moreover you need to answer any queries user has regarding to cooking receipes. 
+You can use tools whenevery necessary. 
+"""
+agent = create_agent(model = model, 
+                     tools = [web_search],
+                    system_prompt = system_prompt)
